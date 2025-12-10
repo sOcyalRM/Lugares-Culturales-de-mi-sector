b32b71b431743a769426ae813965e751d07e1a44
oscaly rosario mateo <oscal@MacBook-Pro.local>
2025-12-09 23:29:52 -0400

    Actualizacion: Se crearon los modelos de base de datos y se hicieron configuraciones en el admin

--- mibarrio/mbbackend/models.py
+++ mibarrio/mbbackend/models.py
@@ -3,4 +3,39 @@ from django.contrib.gis.db import models
 
 # Create your models here.
 class Categoria (models.Model):
-    nombre_categoria = models.Charfield()
+    nombre_categoria = models.CharField('Nombre de Categoria', max_length=50, 
+                                        help_text = 'Especifica la categoria del lugar')
+    creado_en = models.DateTimeField(auto_now_add = True)
+    modificado_en = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        verbose_name_plural = 'Categorias'
+
+    def __str__(self):
+        return self.nombre_categoria
+    
+class Lugar(models.Model):
+    categorias = models.ForeignKey('Categoria', on_delete = models.CASCADE) #Cambiar Categorias por Categoria para evitar error de modelo no existente 
+    nombre_lugar = models.CharField(max_length = 50)
+    descripcion = models.CharField(max_length = 254, blank=True)
+    creado_en = models.DateTimeField(auto_now_add = True)
+    modificado_en = models.DateTimeField(auto_now = True)
+    campo_imagen = models.ImageField (upload_to='imagen_lugares/', blank=True, null=True)
+    activo = models.BooleanField(default = True)
+    punt_geom = models.PointField()
+
+    class Meta:
+        verbose_name_plural = 'Lugares'
+
+    def __str__(self):
+        return self.nombre_lugar
+    
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=50)
+    punt_geom = models.PointField()
+
+    class Meta:
+        verbose_name_plural = 'Ciudades'
+
+    def __str__(self):
+        return self.nombre
--- mibarrio/mibarrio/urls.py
+++ mibarrio/mibarrio/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings            #Importo aqui
+from django.conf.urls.static import static  #importo aqui tambien
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) #Agrego esta linea y las importaciones anteriores para visualizar
+                                                                 # imagenes en el portal del admin
